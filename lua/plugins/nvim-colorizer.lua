6ca8890149072fbeb183b6e195b6afaaf3394676
imck037 <chandradhar.2003@gmail.com>
2026-01-25 19:53:31 +0530

    colorizer set to off by default

--- lua/plugins/nvim-colorizer.lua
+++ lua/plugins/nvim-colorizer.lua
@@ -1,5 +1,11 @@
 return {
   'norcalli/nvim-colorizer.lua',
+  lazy = true,
+  cmd = {
+    'ColorizerToggle',
+    'ColorizerAttachToBuffer',
+    'ColorizerDetachFromBuffer',
+  },
   config = function()
     require('colorizer').setup()
   end,
