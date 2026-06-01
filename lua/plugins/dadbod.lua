ae8b10d1b76d20daf7e22a388554e1f0c36d8a01
Chandradhar Kumar <chandradhar.2003@gmail.com>
2026-06-01 09:14:04 +0530

    ...

---
+++ lua/plugins/dadbod.lua
@@ -0,0 +1,3 @@
+vim.pack.add { 'https://tpope.io/vim/dadbod.git' }
+vim.pack.add { 'https://github.com/kristijanhusak/vim-dadbod-ui' }
+vim.pack.add { 'https://github.com/kristijanhusak/vim-dadbod-completion' }
